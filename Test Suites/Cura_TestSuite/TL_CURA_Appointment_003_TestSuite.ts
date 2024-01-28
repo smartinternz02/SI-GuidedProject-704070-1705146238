3b348dd721c3b79dc60ec8859a84ee301930e787
shashank s <shashank s@shashank_s>
2024-01-28 14:15:19 +0530

    Added New Project

---
+++ Test Suites/Cura_TestSuite/TL_CURA_Appointment_003_TestSuite.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TL_CURA_Appointment_003_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cc0625e8-53f3-4098-be71-216c78e16041</testSuiteGuid>
+   <testCaseLink>
+      <guid>4481bafc-47ff-4c8e-8a86-005025a3ae7e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Listener/TC_Cura_Make_Appointment_003</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d318a4e0-70fb-4d6c-8dc5-a7ad8ba53846</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
