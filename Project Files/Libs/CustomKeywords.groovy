410a32553be00dff84cdc9089181bc7640c5a288
shashanks20 <108606502+shashanks20@users.noreply.github.com>
2024-01-31 00:55:43 +0530

    Updated Project Files

---
+++ Project Files/Libs/CustomKeywords.groovy
@@ -0,0 +1,23 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+
+def static "com.qa.test.customFunctions.printHello"() {
+    (new com.qa.test.customFunctions()).printHello()
+}
+
+
+def static "com.qa.test.customFunctions.CheckDropDownListElementExist"(
+    	TestObject object	
+     , 	String option	) {
+    (new com.qa.test.customFunctions()).CheckDropDownListElementExist(
+        	object
+         , 	option)
+}
---
+++ Project Files/Test Suites/Cura_TestSuite/TC_001-004-005_TestSuite.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TC_001-004-005_TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>5</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>58399624-69a6-4e26-9fae-9c6486dd4a8d</testSuiteGuid>
+   <testCaseLink>
+      <guid>cf8603dd-cb2c-4b46-8828-86863201b4cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buttons/TC_Cura_Home_Launch_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8ad7b057-9500-4918-9178-7c83bd083a0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buttons/TC_Cura_History_004</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9a983258-9fab-4b04-afa6-b70334a039f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buttons/TC_Cura_Home_Logout_005</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
